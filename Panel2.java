378aad2d3a5d7f548435a45e93aa1ac240432359
ws8149 <weisheng3725@gmail.com>
2018-07-19 19:12:33 +0800

    changed panel2 so that it uses GridLayout instead

--- Panel2.java
+++ Panel2.java
@@ -37,71 +37,99 @@ public class Panel2 extends JPanel
         scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
         scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);               
         add(scrollPane, BorderLayout.CENTER);           
-       
-                
-        //create bottom panel (1st and 2nd Nest)
-        JPanel bottom = new JPanel();
-        bottom.setLayout(new BorderLayout());
-        JPanel statsTopBox = new JPanel();
-        statsTopBox.setLayout(new BorderLayout());
-        JPanel statsBottomBox = new JPanel();
-        statsBottomBox.setLayout(new BorderLayout());
-      
-        //create 4 statsbox (3rd nest)
-        JPanel statsTopLeft = new JPanel();
-        statsTopLeft.setLayout(new BorderLayout());
-        JPanel statsTopRight = new JPanel();
-        statsTopRight.setLayout(new BorderLayout());
-        JPanel statsBottomRight = new JPanel();
-        statsBottomRight.setLayout(new BorderLayout());
-        JPanel statsBottomLeft = new JPanel();
-        statsBottomLeft.setLayout(new BorderLayout());
         
-        JPanel statsbox1 = new JPanel();
-        statsbox1.setLayout(new BorderLayout());
-        JPanel statsbox2 = new JPanel();
-        statsbox2.setLayout(new BorderLayout());
-        JPanel statsbox3 = new JPanel();
-        statsbox3.setLayout(new BorderLayout());
-        JPanel statsbox4 = new JPanel();
-        statsbox4.setLayout(new BorderLayout());
-                
+        JPanel bottom = new JPanel();
+        bottom.setLayout(new GridLayout(2,2));
+        
+        
+        //create 4 statsbox        
         StatisticsBox stat1 = new StatisticsBox(1, stats);
         StatisticsBox stat2 = new StatisticsBox(2, stats);
         StatisticsBox stat3 = new StatisticsBox(3, stats);
         StatisticsBox stat4 = new StatisticsBox(4, stats);
         
+        JPanel statsTopLeft = new JPanel();        
+        JPanel statsTopRight = new JPanel();        
+        JPanel statsBottomRight = new JPanel();        
+        JPanel statsBottomLeft = new JPanel();
         
-        statsbox1.setPreferredSize(new Dimension(920,200));
-        statsbox1.add(stat1);
+        statsTopLeft.add(stat1);
+        statsTopRight.add(stat2);
+        statsBottomRight.add(stat3);
+        statsBottomLeft.add(stat4);
         
-        statsbox2.setPreferredSize(new Dimension(920,200));
-        statsbox2.add(stat2);
-        
-        statsbox3.setPreferredSize(new Dimension(920,200));
-        statsbox3.add(stat3);
-        
-        statsbox4.setPreferredSize(new Dimension(920,200));
-        statsbox4.add(stat4);
-        
-        bottom.setBorder(BorderFactory.createEtchedBorder(EtchedBorder.LOWERED));
-        statsbox1.setBorder(BorderFactory.createEtchedBorder(EtchedBorder.RAISED));
-        statsbox2.setBorder(BorderFactory.createEtchedBorder(EtchedBorder.RAISED));
-        statsbox3.setBorder(BorderFactory.createEtchedBorder(EtchedBorder.RAISED));
-        statsbox4.setBorder(BorderFactory.createEtchedBorder(EtchedBorder.RAISED));
+        bottom.add(statsTopLeft);
+        bottom.add(statsTopRight);
+        bottom.add(statsBottomLeft);
+        bottom.add(statsBottomRight);       
         
         add(bottom, BorderLayout.SOUTH);
-        bottom.add(statsTopBox, BorderLayout.NORTH);
-        statsTopBox.add(statsTopLeft, BorderLayout.WEST);
-        statsTopBox.add(statsTopRight, BorderLayout.EAST);
-        statsTopRight.add(statsbox2);
-        statsTopLeft.add(statsbox1);
-        
-        bottom.add(statsBottomBox, BorderLayout.SOUTH);
-        statsBottomBox.add(statsBottomLeft, BorderLayout.WEST);
-        statsBottomBox.add(statsBottomRight, BorderLayout.EAST);
-        statsBottomRight.add(statsbox4);
-        statsBottomLeft.add(statsbox3);        
+                
+//        //create bottom panel (1st and 2nd Nest)
+//        JPanel bottom = new JPanel();
+//        bottom.setLayout(new BorderLayout());
+//        JPanel statsTopBox = new JPanel();
+//        statsTopBox.setLayout(new BorderLayout());
+//        JPanel statsBottomBox = new JPanel();
+//        statsBottomBox.setLayout(new BorderLayout());
+//        
+//        
+//      
+//        //create 4 statsbox (3rd nest)
+//        JPanel statsTopLeft = new JPanel();
+//        statsTopLeft.setLayout(new BorderLayout());
+//        JPanel statsTopRight = new JPanel();
+//        statsTopRight.setLayout(new BorderLayout());
+//        JPanel statsBottomRight = new JPanel();
+//        statsBottomRight.setLayout(new BorderLayout());
+//        JPanel statsBottomLeft = new JPanel();
+//        statsBottomLeft.setLayout(new BorderLayout());
+//        
+//        JPanel statsbox1 = new JPanel();
+//        statsbox1.setLayout(new BorderLayout());
+//        JPanel statsbox2 = new JPanel();
+//        statsbox2.setLayout(new BorderLayout());
+//        JPanel statsbox3 = new JPanel();
+//        statsbox3.setLayout(new BorderLayout());
+//        JPanel statsbox4 = new JPanel();
+//        statsbox4.setLayout(new BorderLayout());
+//                
+//        StatisticsBox stat1 = new StatisticsBox(1, stats);
+//        StatisticsBox stat2 = new StatisticsBox(2, stats);
+//        StatisticsBox stat3 = new StatisticsBox(3, stats);
+//        StatisticsBox stat4 = new StatisticsBox(4, stats);
+//        
+//        
+//        statsbox1.setPreferredSize(new Dimension(920,200));
+//        statsbox1.add(stat1);
+//        
+//        statsbox2.setPreferredSize(new Dimension(920,200));
+//        statsbox2.add(stat2);
+//        
+//        statsbox3.setPreferredSize(new Dimension(920,200));
+//        statsbox3.add(stat3);
+//        
+//        statsbox4.setPreferredSize(new Dimension(920,200));
+//        statsbox4.add(stat4);
+//        
+//        bottom.setBorder(BorderFactory.createEtchedBorder(EtchedBorder.LOWERED));
+//        statsbox1.setBorder(BorderFactory.createEtchedBorder(EtchedBorder.RAISED));
+//        statsbox2.setBorder(BorderFactory.createEtchedBorder(EtchedBorder.RAISED));
+//        statsbox3.setBorder(BorderFactory.createEtchedBorder(EtchedBorder.RAISED));
+//        statsbox4.setBorder(BorderFactory.createEtchedBorder(EtchedBorder.RAISED));
+//        
+//        add(bottom, BorderLayout.SOUTH);
+//        bottom.add(statsTopBox, BorderLayout.NORTH);
+//        statsTopBox.add(statsTopLeft, BorderLayout.WEST);
+//        statsTopBox.add(statsTopRight, BorderLayout.EAST);
+//        statsTopRight.add(statsbox2);
+//        statsTopLeft.add(statsbox1);
+//        
+//        bottom.add(statsBottomBox, BorderLayout.SOUTH);
+//        statsBottomBox.add(statsBottomLeft, BorderLayout.WEST);
+//        statsBottomBox.add(statsBottomRight, BorderLayout.EAST);
+//        statsBottomRight.add(statsbox4);
+//        statsBottomLeft.add(statsbox3);        
         
         
     }
